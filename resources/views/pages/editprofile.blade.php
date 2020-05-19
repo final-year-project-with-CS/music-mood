0bebd6c09f3e5ebd901726f003327efdcdacae84
anordy <anordypatrick98@gmail.com>
2020-05-19 13:33:44 +0300

    change

--- resources/views/pages/editprofile.blade.php
+++ resources/views/pages/editprofile.blade.php
@@ -27,11 +27,11 @@
           <ul class="links d-flex align-items-center mt-3 mt-md-0">
             <li class="header-link-item d-flex align-items-center active">
               <i class="mr-1 icon-md" data-feather="columns"></i>
-              <a class="pt-1px d-none d-md-block" href="#">Timeline</a>
+              <a class="pt-1px d-none d-md-block" href="#collapsetimeline" data-toggle="collapse" >Timeline</a>
             </li>
             <li class="header-link-item ml-3 pl-3 border-left d-flex align-items-center">
               <i class="mr-1 icon-md" data-feather="user"></i>
-              <a class="pt-1px d-none d-md-block" href="#">About</a>
+              <a class="pt-1px d-none d-md-block" href="#collapseAbout" data-toggle="collapse">About</a>
             </li>
             <li class="header-link-item ml-3 pl-3 border-left d-flex align-items-center">
               <i class="mr-1 icon-md" data-feather="users"></i>
@@ -46,6 +46,29 @@
               <a class="pt-1px d-none d-md-block" href="#">Videos</a>
             </li>
           </ul>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12">
+         <div class="card">
+           <div class="collapse" id="collapsetimeline">
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptas repudiandae nostrum obcaecati iusto ducimus saepe, vitae sapiente perspiciatis magni quas facilis mollitia ipsa facere quam culpa eligendi illum laudantium, nemo odio maiores cupiditate aliquid. Odio reprehenderit deserunt voluptate alias autem vitae repellat non nobis minima amet! Quia quam labore deleniti laborum porro, omnis, neque assumenda esse cumque et aliquid!
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, quasi nostrum aliquid sint vitae aspernatur asperiores delectus officiis possimus illum quo odio provident dolorum, dolores tempora, cumque temporibus totam veniam? Iusto numquam qui eligendi fugiat laboriosam molestias necessitatibus blanditiis incidunt minima. Quisquam nesciunt distinctio deleniti voluptates quos, quia iure culpa iusto consequatur in quas fugit. Culpa aliquid nisi voluptas reiciendis!
+           </div>
+         </div>
+        </div>
+      </div>
+
+
+      <div class="row ">
+        <div class="col-lg-12">
+         <div class="card">
+           <div class="collapse" id="collapseAbout">
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptas repudiandae nostrum obcaecati iusto ducimus saepe, vitae sapiente perspiciatis magni quas facilis mollitia ipsa facere quam culpa eligendi illum laudantium, nemo odio maiores cupiditate aliquid. Odio reprehenderit deserunt voluptate alias autem vitae repellat non nobis minima amet! Quia quam labore deleniti laborum porro, omnis, neque assumenda esse cumque et aliquid!
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, quasi nostrum aliquid sint vitae aspernatur asperiores delectus officiis possimus illum quo odio provident dolorum, dolores tempora, cumque temporibus totam veniam? Iusto numquam qui eligendi fugiat laboriosam molestias necessitatibus blanditiis incidunt minima. Quisquam nesciunt distinctio deleniti voluptates quos, quia iure culpa iusto consequatur in quas fugit. Culpa aliquid nisi voluptas reiciendis!
+           </div>
+         </div>
         </div>
       </div>
     </div>
