a632c4ff6afe9bd28b1ae27f6d32461a362c82ac
ckabelwa2468 <ckabelwa@gmail.com>
2020-05-24 23:48:26 +0300

    user factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,9 +20,13 @@ use Illuminate\Support\Str;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'artist_nickname' => $faker->name,
+        'date_of_birth'=>$faker->date,
+        'avatar'=>$faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'isactive'=>$faker->boolean,
     ];
 });
